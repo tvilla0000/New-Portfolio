4c79ffe697875326ad06a39c7727a23cc3a3c062
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-09-03 18:41:30 -0700

    Delete 1920 Barbers and Pokedex from project data

--- models/project.js
+++ models/project.js
@@ -45,30 +45,6 @@ let projects = [
     imgUrl: "https://i.imgur.com/TgFOa5V.png"
   },
   {
-    name: "1920 Barbers",
-    id: "barbers",
-    type: "Frontend",
-    technology: [
-      {
-        name: "HTML",
-        techImgUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/600px-HTML5_Badge.svg.png"
-      },
-      {
-        name: "CSS",
-        techImgUrl: "https://i.ya-webdesign.com/images/css-3-logo-png-7.png"
-      },
-      {
-        name: "Javascript",
-        techImgUrl:
-          "https://www.geekstrick.com/wp-content/themes/geekstrick/assets/icons/javascript.svg"
-      }
-    ],
-    description:
-      "1920 Barbers is a site created for the 1920 Barbershop in Concord, CA. The site makes it more convenient for clients to set up appointments",
-    imgUrl: "https://i.imgur.com/W69pXVu.png"
-  },
-  {
     name: "WeStream",
     id: "westream",
     type: "Fullstack",
@@ -89,7 +65,7 @@ let projects = [
       }
     ],
     description:
-      "WeStream is an Full-stack application meant to mimic Youtube's video streaming services! ",
+      "WeStream is an Full-stack application meant which implements Youtube's Data API to mimic its streaming services!",
     imgUrl: "https://i.imgur.com/NeCJEHh.png"
   },
   {
@@ -117,26 +93,7 @@ let projects = [
       "This app was created as a project which allows users to create a profile and create blog posts for others to see!",
     imgUrl: "https://i.ibb.co/Jc5KzCX/Screen-Shot-2020-01-18-at-10-50-21-PM.png"
   },
-  {
-    name: "Pokedex",
-    id: "pokedex",
-    type: "Frontend",
-    technology: [
-      {
-        name: "React",
-        techImgUrl: "http://maxehnert.com/app/images/react.svg"
-      },
 
-      {
-        name: "Javascript",
-        techImgUrl:
-          "https://www.geekstrick.com/wp-content/themes/geekstrick/assets/icons/javascript.svg"
-      }
-    ],
-    description:
-      "Frontend project using React and Javascript meant to replicate a Pokedex that implements the use of type ahead search!",
-    imgUrl: "https://i.ibb.co/10nxq2Y/Screen-Shot-2020-01-18-at-10-38-23-PM.png"
-  },
   {
     name: "Node",
     id: "node-master",
@@ -172,9 +129,7 @@ const isFullstack = projects.filter(x => {
 });
 
 const recentProjects = projects.filter(x => {
-  return (
-    x.name === "WeStream" || x.name === "Supe API" || x.name === "1920 Barbers"
-  );
+  return x.name === "WeStream" || x.name === "Supe API" || x.name === "Tasty";
 });
 
 const getAllFrontend = () => {
